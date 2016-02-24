2098fd52fc86e6259f029940070d43a8962b27d4
witzke <leanwitzke@gmail.com>
2016-02-24 17:20:45 -0300

    Agregado Paper y reestructuracion del proyecto

---
+++ src/enfoqueponderado.py
@@ -0,0 +1,11 @@
+def eliminacion_palabras_parada:
+    return "Nada por ahora"
+
+def stemming:
+    return "Nada por ahora"
+
+def tokenization:
+    return "Nada por ahora"
+
+def construir_diccionario_dominio:
+    return "Nada por ahora"
